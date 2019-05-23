85ff23451dcecaf854b8d32d7fce6775655fe470
Shortcake111 <vdhlies@gmail.com>
2019-05-23 14:46:21 +0200

    Implemented better way to load reviews and their cards

---
+++ js/reviews/bigBoy.js
@@ -0,0 +1,21 @@
+var titles = ["Title", "Story", "Characters", "Art", "Music", "Sexual content", "Summary"];
+var content = ["BigBoy", "In this very short romance-themed visual novel, you play as a ♀️ (1st person) who is entering a new school. " +
+    "There are 2 girls to choose from, but you're not forced to get into a romantic relationship with any of them. " +
+    "Unfortunately, you don't get to spend much time with anyone. The main events pass by very quickly, while the in-between moments are usually just a few sentences long as if they're almost asking to be ignored. " +
+    "Unless you're just aiming for the sexual scenes (which are also very short), the pace of the story is too high. " +
+    "Which caused me to not feel much of the romance factor. <br /><b>Gameplay</b><br /> You'll come across lots of choices, of which every single one has an impact. " +
+    "There are multiple endings to unlock. The game considers all of them as good, although most of them didn't leave me with the typical happy ending feeling. <br /><b>Language</b><br /> " +
+    "The English is understandable, but I found some sentences a bit confusing (e.g. using the word 'girlfriend' for a female friend).",
+    "The characters disappointed me as well. Maybe it's just because of the story's tempo, but even if they were supposed to be stereotypes, they seemed too flat to me. they really lack expressiveness. " +
+    "Even after some critical moments or choices <spoiler>(like leaving a \"goodbye\" letter)</spoiler>, the characters just easily go on with their lives, almost completely ignoring what just happened. " +
+    "At least you still get to know a few things about one of the girls, but you unlock the \"actual scenes\" and endings way too quickly/easily.",
+    "The anime-styled art is great. The backgrounds are really colorful. The characters are a bit different in style (more chibi-like?) but very cute. ^.^ I particularly like all the soft colors everywhere.",
+    "I would say the classic music is just a background filler here: not out of place, but it doesn't really change the experiences that much.",
+    "There are 2 sexual scenes to unlock, both easy to get and a bit short I should say. Only nipples are visible sometimes. There's one moment without nudity that's also a bit sexual.",
+    "<table><tr><th>Story</th><td>😕</td></tr><tr><th>Characters</th><td>😒</td></tr><tr><th>Art</th><td>😃</td></tr><tr><th>Music</th><td>🙂</td></tr><tr><th>Sexual content</th><td>😕</td></tr></table>",
+    "Even though the art was nice, I really missed the emotions that visual novels I like bring me in. All in all, I don't regret paying for it. Apart from the text, they probably put a lot of effort into the art. I'm going to use a few images as backgrounds for sure."
+];
+var twitter = ["https://twitter.com/MeteOzguz?ref_src=twsrc%5Etfw", "Follow @MeteOzguz"];
+var bigBoy = [titles, content, twitter, "https://store.steampowered.com/widget/825550/"];
+
+loadReview(bigBoy)
